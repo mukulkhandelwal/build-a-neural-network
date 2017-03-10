57bae0fcf0bd629daea0e31e27f731b7e3cbc5ea
Mukul <you@example.com>
2017-03-10 21:59:44 +0530

    all import added and LSTM read file added

---
+++ Neural/recurrent.py
@@ -0,0 +1,31 @@
+#neural network recurrent network
+#generate new city names
+
+#LSTM("long sort term memoty ") NN to generate city names
+from __future inport absolute import , divison , print fucntion
+
+import os 
+from six import moves #url like module , pull data from url
+
+import ssl
+import tflearn
+from tflearn.data_utils import 
+
+#Step 1 - Retrieve the data 
+
+path = "US_cities.txt"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
